6e597af53bf8df0e781b73d2b518ec04b8c761ba
TadeuA <tadeu01399@gmail.com//>
2020-03-29 13:31:44 -0300

    backend part1

--- theBeHero/backend/src/index.js
+++ theBeHero/backend/src/index.js
@@ -1,9 +1,15 @@
 //importando funcionalidades de modulos
 const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
 
 //instanciando aplicação
 const PORT = 3333;
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT);
